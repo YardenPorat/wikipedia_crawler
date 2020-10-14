20614bc17e9236e502841e08d3ea96b487c61740
Yarden Porat <yardenporat@gmail.com>
2020-10-14 20:01:21 +0300

    2nd

--- main.py
+++ main.py
@@ -1,51 +1,70 @@
-import urllib.request
+import requests
 
 
 class Crawler:
+    NAME = 'Wikipedia Crawler'
+    URL_START = 'https://en.wikipedia.org/wiki/'
+    CLEANER = ['#', 'class=', ':', '=', 'Category', '/']
+
     def __init__(self, url):
-        self.name = 'Wikipedia Crawler'
         self.url = url
-        self.url_start = 'https://en.wikipedia.org/wiki/'
-        self.cleaner = ['#', 'class=', ':', '=', 'Category', '/']
 
-    def parse(self):
-        link_data = urllib.request.urlopen(self.url)
-        my_file = str(link_data.read())
+    @staticmethod
+    def get_page_content(url):
+        response = requests.get(url)
+        return response.text
 
+    @staticmethod
+    def cut_relevant_part(content):
         # clean before start and after end
-        start = my_file.index('bodyContent')
-        end = my_file.index('mw-data-after-content')
-        my_file = my_file[start:end]
+        start = content.index('bodyContent')
+        end = content.index('mw-data-after-content')
+        return content[start:end]
 
-        # split to a list
-        lst = my_file.split(r'href=')
+    def extract_refs(self, content):
+
+        lst = content.split(r'href=')
         lst2 = []
+        HREF_SEPARATOR = '"'
+
         for segment in lst:
-            separator = '"'
-            cutoff = self.find_2nd(segment, separator)
+            cutoff = self.find_2nd(segment, HREF_SEPARATOR)
             lst2.append(segment[:cutoff])
 
         lst = []
+
+        URL_SEPARATOR = '/'
         for page in lst2:
-            separator = '/'
-            cutoff = self.find_2nd(page, separator)
+            cutoff = self.find_2nd(page, URL_SEPARATOR)
             page = page[cutoff + 1:]
-            if any(clean in page for clean in self.cleaner):
+            if any(clean in page for clean in self.CLEANER):
                 continue
             else:
                 lst.append(page)
-        print(lst)
+        return lst
 
+    def insert_to_dict(self, refs):
         # put in dict
         res = {}
-        for item in lst:
-            res[item] = self.url_start+item
-
+        for item in refs:
+            res[item] = self.URL_START + item
         return res
 
-    def find_2nd(self, string, substring):
+    def parse(self):
+        print(f'{self.NAME} started!')
+        content = self.get_page_content(self.url)
+        topic_body_content = self.cut_relevant_part(content)
+        refs = self.extract_refs(topic_body_content)
+        res = self.insert_to_dict(refs)
+
+        print(f'{self.NAME} finished!')
+        return res
+
+    @staticmethod
+    def find_2nd(string, substring):
         return string.find(substring, string.find(substring) + 1)
 
 
-f = Crawler("https://en.wikipedia.org/wiki/Misliya_cave")
-print(f.parse())
+if __name__ == '__main__':
+    f = Crawler("https://en.wikipedia.org/wiki/Misliya_cave")
+    print(f.parse())
